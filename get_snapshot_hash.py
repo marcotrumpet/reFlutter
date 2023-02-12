1e408ff733ea1d64195fa7ec3f1b6b2c592c0815
Jay <jayluxferro@sperixlabs.org>
2023-02-12 12:54:20 +0000

    Code formatting

--- get_snapshot_hash.py
+++ get_snapshot_hash.py
@@ -4,9 +4,10 @@
 Get snapshot hash
 """
 
-import sys
-import string
 import re
+import string
+import sys
+
 
 def usage():
     print('[-] Usage: python {} [lib_app_library]'.format(sys.argv[0]))
@@ -17,7 +18,7 @@ if len(sys.argv) != 2:
     usage()
 
 fname = sys.argv[1]
-min=32
+min = 32
 if sys.version_info >= (3, 0):
     f = open(fname, errors="ignore")
 else:
@@ -30,11 +31,11 @@ for c in f.read():
         result += c
         continue
     if len(result) >= min:
-       hashT = re.findall(r"([a-f\d]{32})", result)
-       if(len(hashT) > 0):
-           libappHash = hashT[0]
-           break
-       f.close()
+        hashT = re.findall(r"([a-f\d]{32})", result)
+        if len(hashT) > 0:
+            libappHash = hashT[0]
+            break
+        f.close()
     result = ""
 
 print(libappHash)
